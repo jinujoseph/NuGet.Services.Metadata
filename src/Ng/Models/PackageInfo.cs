daa03dadb9e36079a6dd6e6bea6fd2d53f2465bf
rtaket <rtaket@microsoft.com>
2016-02-08 15:51:55 -0800

    Add missing files.

---
+++ src/Ng/Models/PackageInfo.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ng.Models
+{
+    public class PackageInfo
+    {
+        [JsonProperty("id")]
+        public string PackageId
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("latestStableVersion")]
+        public string LatestStableVersion
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("commitId")]
+        public Guid CommitId
+        {
+            get;
+            set;
+        }
+    }
+}
+
---
+++ src/Ng/Persistence/JsonStorageItem.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using NuGet.Services.Metadata.Catalog.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ng.Persistence
+{
+    abstract class JsonStorageItem
+    {
+        protected JsonStorage(Uri address, IStorage storage)
+        {
+            this.Address = address;
+            this.Storage = storage;
+        }
+
+        [JsonIgnore]
+        public Uri Address
+        {
+            get;
+            protected set;
+        }
+
+        [JsonIgnore]
+        public IStorage Storage
+        {
+            get;
+            protected set;
+        }
+
+        public abstract Task SaveAsync(CancellationToken cancellationToken);
+
+        public abstract Task LoadAsync(Uri address, IStorage storage, CancellationToken cancellationToken);
+    }
+}
+
