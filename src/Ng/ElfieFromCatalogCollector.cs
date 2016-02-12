f156d3b32cd8a21689bfd72e013eab6f8981d8a6
rtaket <rtaket@microsoft.com>
2016-02-11 17:49:24 -0800

    Added comments.

--- src/Ng/ElfieFromCatalogCollector.cs
+++ src/Ng/ElfieFromCatalogCollector.cs
@@ -113,11 +113,15 @@ namespace Ng
             {
                 Trace.TraceInformation("Processing package {0}", package.CatalogEntry.PackageId);
 
+                // Get the storage url for the idx file. We'll use this to check if the 
+                // idx file already exists before going through the effort of creating one.
                 Uri idxResourceUri = this._storage.ComposeIdxResourceUrl(this._indexerVersion, package.CatalogEntry.PackageId, package.CatalogEntry.PackageVersion);
                 StorageContent idxStorageItem = this._storage.Load(idxResourceUri, new CancellationToken()).Result;
 
                 if (idxStorageItem == null)
                 {
+                    // We didn't have the idx file in storage, so go through the process of downloading,
+                    // decompressing and creating the idx file.
                     ProcessPackageDetailsAsync(package, cancellationToken).Wait();
                 }
                 else
@@ -240,7 +244,7 @@ namespace Ng
                 }
                 else
                 {
-                    Trace.TraceInformation("Saving the idx file.");
+                    Trace.TraceInformation($"Saving the idx file. {idxFile}");
 
                     idxResourceUri = this._storage.ComposeIdxResourceUrl(this._indexerVersion, package.CatalogEntry.PackageId, package.CatalogEntry.PackageVersion);
                     this._storage.SaveFileContents(idxFile, idxResourceUri);
@@ -258,8 +262,18 @@ namespace Ng
                 // The idx creation failed, so delete any files which were saved to storage.  
                 if (idxResourceUri != null)
                 {
-                    await this._storage.Delete(idxResourceUri, cancellationToken);
-                    idxResourceUri = null;
+                    try
+                    {
+                        await this._storage.Delete(idxResourceUri, cancellationToken);
+                        idxResourceUri = null;
+                    }
+                    catch (Exception e)
+                    {
+                        // The resource couldn't be deleted.
+                        // Log the error and continue. The original exception will be rethrown.
+                        Trace.TraceWarning($"Could not delete idx from storage.");
+                        Trace.TraceWarning(e.ToString());
+                    }
                 }
 
                 throw;
@@ -314,6 +328,12 @@ namespace Ng
             return idxFile;
         }
 
+        /// <summary>
+        /// Downloads the json file which contains the package download counts.
+        /// </summary>
+        /// <param name="downloadJsonUri">The url to the file which contains the download counts.</param>
+        /// <returns>A JArray representing the downloaded file.</returns>
+        /// <remarks>The file downloaded is a json array, not a json file. i.e. it is not enclosed in { }.</remarks>
         JArray FetchDownloadCounts(Uri downloadJsonUri)
         {
             Trace.TraceInformation("Downloading download json file.");
@@ -336,6 +356,12 @@ namespace Ng
             return downloadJson;
         }
 
+        /// <summary>
+        /// Finds all the packages which should be included in the ardb file. 
+        /// </summary>
+        /// <param name="downloadJson">The json file which contains the download counts for all the of NuGet packages.</param>
+        /// <param name="downloadPercentage">The percentage of downloads to include in the ardb file.</param>
+        /// <returns></returns>
         IEnumerable<RegistrationIndexPackage> GetPackagesToInclude(JArray downloadJson, double downloadPercentage)
         {
             long totalDownloadCount;
@@ -346,13 +372,20 @@ namespace Ng
             // Calculate the download threshold
             long downloadCountThreshold = (long)(totalDownloadCount * downloadPercentage);
 
-            // Filter and sort the packages to only the ones we want to include. 
+            // Filter and sort the packages to only the ones we want to include. i.e. remove any packages which do
+            // not have a latest stable version and only take packages within the download threshold.
             // Note: packagesToInclude is already sorted in the order the packages should appear in the ardb index.
-            IEnumerable<RegistrationIndexPackage> packagesToInclude = GetPackagesToInclude(packageDownloadCounts, downloadCountThreshold);
+            IEnumerable<RegistrationIndexPackage> packagesToInclude = FilterPackagesToInclude(packageDownloadCounts, downloadCountThreshold);
 
             return packagesToInclude;
         }
 
+        /// <summary>
+        ///  Reads the JArray containing the package download counts and converts it to
+        ///  a dictionary of package name to download count.
+        /// </summary>
+        /// <param name="downloadJson">The JArray of package download counts.</param>
+        /// <param name="totalDownloadCount">The sum of all the package download counts.</param>
         Dictionary<string, long> GetDownloadsPerPackage(JArray downloadJson, out long totalDownloadCount)
         {
             totalDownloadCount = 0;
@@ -363,7 +396,7 @@ namespace Ng
             {
                 string packageId = packageDownloadJson.First.Value<string>();
 
-                // Get the package counts.  
+                // Sum the downloads for each version of the package.
                 long packageDownloadCount = 0;
                 foreach (JArray versionDownloadJson in packageDownloadJson.Children<JArray>())
                 {
@@ -377,11 +410,17 @@ namespace Ng
                 totalDownloadCount += packageDownloadCount;
             }
 
+            Trace.TraceInformation($"Total pacakge count from download json: {packageDownloadCounts.Count.ToString("#,###")}");
             Trace.TraceInformation($"Total download count: {totalDownloadCount.ToString("#,###")}");
 
             return packageDownloadCounts;
         }
 
+        /// <summary>
+        /// Gets the latest stable version of a set of packages from the NuGet registration service.
+        /// </summary>
+        /// <param name="packageIds">The list of package ids.</param>
+        /// <returns>A mapping of package ids to its corresponding registration metadata.</returns>
         Dictionary<string, RegistrationIndexPackage> GetLatestStableVersion(IEnumerable<string> packageIds)
         {
             Dictionary<string, RegistrationIndexPackage> latestStableVersions = new Dictionary<string, RegistrationIndexPackage>();
@@ -404,6 +443,11 @@ namespace Ng
             return latestStableVersions;
         }
 
+        /// <summary>
+        /// Gets the latest stable version of a package.
+        /// </summary>
+        /// <param name="packageId">The package id.</param>
+        /// <returns>The registration information for the latest stable version of a package.</returns>
         RegistrationIndexPackage GetLatestStableVersion(string packageId)
         {
             Trace.TraceInformation($"Determining latest stable version {packageId}.");
@@ -443,22 +487,48 @@ namespace Ng
             return latestStableVersion;
         }
 
-        IEnumerable<RegistrationIndexPackage> GetPackagesToInclude(Dictionary<string, long> packageDownloadCounts, long downloadCountThreshold)
+        /// <summary>
+        /// Filters the package list to only the latest stable version of the packages
+        /// </summary>
+        /// <param name="packageDownloadCounts"></param>
+        /// <param name="downloadCountThreshold"></param>
+        /// <returns></returns>
+        IEnumerable<RegistrationIndexPackage> FilterPackagesToInclude(Dictionary<string, long> packageDownloadCounts, long downloadCountThreshold)
         {
+            // In general, here's how the filtering works.
+            //   1. Calcuate the total download count for all the NuGet packages. (this is done before calling this method.)
+            //   2. Calculate how many downloads we want to include in the ardb. (this is done before calling this method.)
+            //   3. Exclude any packages which do not have a latest stable version.
+            //   4. Sort the packages based on download count.
+            //   5. Include the most popular packages until the target download count is reached.
+            //   6. Sort the included packages, first by log2(downloadcount), then by package name.
+            //      This sort order ensures the most popular packages are recommended first, while
+            //      minimizing the day-to-day differences.
+
+
+            // The runninng count of the downloads included in the ardb.
             long includedDownloadCount = 0;
+
+            // A flag to signal that the download threshold was reached.
             bool thresholdReached = false;
             List<Tuple<RegistrationIndexPackage, int>> includedPackagesWithCounts = new List<Tuple<RegistrationIndexPackage, int>>();
 
+            // We need to determine the latest stable version for each package. But this is a fairly expensive operation since
+            // it makes a network call. We'll process the packages in chuncks so we can get the latest stable version of
+            // the packages using multiple threads.
             int batchSize = 500;
             int currentPosition = 0;
 
+            // We need to process the packages in order from most popular to least popular. (item.Value is the download count for the package)
             var orderedDownloadCounts = packageDownloadCounts.OrderByDescending(item => item.Value);
             IEnumerable<string> batch = null;
 
             do
             {
+                // Get the next chunk of packages to process.
                 batch = orderedDownloadCounts.Skip(currentPosition).Take(batchSize).Select(item => item.Key);
 
+                // Get the latest stable versions of the packages.
                 Dictionary<string, RegistrationIndexPackage> latestStableVersions = GetLatestStableVersion(batch);
 
                 foreach (string packageId in batch)
@@ -466,11 +536,14 @@ namespace Ng
                     long downloadCount = packageDownloadCounts[packageId];
                     RegistrationIndexPackage latestStableVersion;
 
+                    // If there's a latest stable version for the package, we want to include it.
                     if (latestStableVersions.TryGetValue(packageId, out latestStableVersion))
                     {
                         Trace.TraceInformation($"Included package: {packageId} - {downloadCount.ToString("#,####")}");
                         includedDownloadCount += downloadCount;
 
+                        // Calculate the log base 2 version of the download count. We'll use this value to
+                        // sort the packages in the ardb file.
                         int base2Count = 0;
                         if (downloadCount != 0)
                         {
@@ -486,6 +559,7 @@ namespace Ng
                         downloadCountThreshold -= (long)(downloadCount * this._downloadPercentage);
                     }
 
+                    // Stop if we've reached the download threhold.
                     Trace.TraceInformation($"Download count {includedDownloadCount.ToString("#,####")} / {downloadCountThreshold.ToString("#,####")}");
                     thresholdReached = (includedDownloadCount >= downloadCountThreshold);
 
@@ -502,53 +576,36 @@ namespace Ng
 
             Trace.TraceInformation($"Including {includedPackagesWithCounts.Count.ToString("#,###")} packages.");
 
+            // Sort the packages, first by the log base 2 download count, then by the package id.
+            // This ensures the most popular packages are recommended over the less popular packages. It also
+            // reduces the diffs from day-to-day (it reduces the reordering of the packages as download counts change from day-to-day.)
             var sortedIncludedPackages = includedPackagesWithCounts.OrderByDescending(item => item.Item2).ThenBy(item => item.Item1.CatalogEntry.PackageId);
             return sortedIncludedPackages.Select(item => item.Item1);
         }
 
-        IEnumerable<string> StageIdxFiles(IEnumerable<RegistrationIndexPackage> packages, IStorage storage, Version indexerVersion, string outputDirectory)
-        {
-            List<string> idxFileList = new List<string>();
-            Directory.CreateDirectory(outputDirectory);
-
-            foreach (RegistrationIndexPackage package in packages)
-            {
-                Uri idxResourceUri = storage.ComposeIdxResourceUrl(indexerVersion, package.CatalogEntry.PackageId, package.CatalogEntry.PackageVersion);
-
-                using (StorageContent idxContent = storage.Load(idxResourceUri, new CancellationToken()).Result)
-                {
-                    if (idxContent != null)
-                    {
-                        string idxFilePath = Path.Combine(outputDirectory, Path.GetFileName(idxResourceUri.LocalPath));
-                        idxFileList.Add(idxFilePath);
-
-                        using (Stream idxStream = idxContent.GetContentStream())
-                        {
-                            using (FileStream fileStream = File.OpenWrite(idxFilePath))
-                            {
-                                idxStream.CopyTo(fileStream);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return idxFileList;
-        }
-
+        /// <summary>
+        /// Stages the files required to create the ardb and run the merger to produce the ardb.
+        /// </summary>
+        /// <param name="sortedPackagesToInclude">The list of packages to include in the ardb.</param>
+        /// <param name="indexerVersion">The version of the indexer used to create the idx file.</param>
+        /// <param name="mergerVersion">The version of the merger to create the ardb file.</param>
+        /// <param name="outputDirectory">The working directory.</param>
         void CreateArdbFile(IEnumerable<RegistrationIndexPackage> sortedPackagesToInclude, Version indexerVersion, Version mergerVersion, string outputDirectory)
         {
             try
             {
+                // Set up the directory structure.
                 string idxDirectory = Path.Combine(outputDirectory, "idx");
                 string logsDirectory = Path.Combine(outputDirectory, "logs");
                 Directory.CreateDirectory(outputDirectory);
                 Directory.CreateDirectory(idxDirectory);
                 Directory.CreateDirectory(logsDirectory);
 
+                // Stage the files and run the merger.
                 IEnumerable<string> idxList = StageIdxFiles(sortedPackagesToInclude, this._storage, indexerVersion, idxDirectory);
                 string ardbTextFile = RunArdbMerger(mergerVersion, idxList, outputDirectory);
 
+                // Save the ardb/txt file.
                 string version = DateTime.UtcNow.ToString("yyyyMMdd");
                 Uri ardbResourceUri = this._storage.ComposeArdbResourceUrl(mergerVersion, $"{version}\\{version}.ardb.txt");
                 this._storage.SaveFileContents(ardbTextFile, ardbResourceUri);
@@ -566,6 +623,57 @@ namespace Ng
             }
         }
 
+        /// <summary>
+        /// Copies the idx files from storage to the local machine. This is to prep the local machine before running the ardb merger.
+        /// </summary>
+        /// <param name="packages">The list of packages to copy from storage.</param>
+        /// <param name="storage">The storage object that can access the idx files.</param>
+        /// <param name="indexerVersion">The indexer version used to create the idx files.</param>
+        /// <param name="outputDirectory">The directory to copy the idx files to.</param>
+        /// <returns>The local paths to the idx files. This will be used by the ardb merger to identify which packages to include.</returns>
+        IEnumerable<string> StageIdxFiles(IEnumerable<RegistrationIndexPackage> packages, IStorage storage, Version indexerVersion, string outputDirectory)
+        {
+            List<string> localIdxFileList = new List<string>();
+            Directory.CreateDirectory(outputDirectory);
+
+            foreach (RegistrationIndexPackage package in packages)
+            {
+                // This is the URL of the idx file in storage. i.e. this is the source path
+                Uri idxResourceUri = storage.ComposeIdxResourceUrl(indexerVersion, package.CatalogEntry.PackageId, package.CatalogEntry.PackageVersion);
+
+                using (StorageContent idxContent = storage.Load(idxResourceUri, new CancellationToken()).Result)
+                {
+                    if (idxContent != null)
+                    {
+                        // This is the path to the local file. i.e. this is the destination path
+                        string localFilePath = Path.Combine(outputDirectory, Path.GetFileName(idxResourceUri.LocalPath));
+                        localIdxFileList.Add(localFilePath);
+
+                        // Copy the file from storage to the local path
+                        using (Stream idxStream = idxContent.GetContentStream())
+                        {
+                            using (FileStream fileStream = File.OpenWrite(localFilePath))
+                            {
+                                idxStream.CopyTo(fileStream);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Trace.TraceInformation($"Copied {localIdxFileList.Count.ToString("#,###")} idx files from storage to {outputDirectory}");
+
+            return localIdxFileList;
+        }
+
+        /// <summary>
+        /// Runs the merger to create the ardb file.
+        /// </summary>
+        /// <param name="mergerVersion">The version of the merger tool to use to create the ardb file.</param>
+        /// <param name="idxList">The list of idx file to include in the ardb file. This list must be sorted in priority order.
+        /// i.e. the packages at the top of the list will be recommended over the packages at the bottom of the list.</param>
+        /// <param name="outputDirectory">The directory to save the ardb file to.</param>
+        /// <returns></returns>
         string RunArdbMerger(Version mergerVersion, IEnumerable<string> idxList, string outputDirectory)
         {
             Directory.CreateDirectory(outputDirectory);
