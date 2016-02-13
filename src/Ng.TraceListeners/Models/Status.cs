bc190211a58803ccae5e6f7857bcd57e42093a81
Ryley Taketa <rtaket@microsoft.com>
2016-02-12 19:51:46 -0800

    Add a trace listener that writes status to an azure storage table.

---
+++ src/Ng.TraceListeners/Models/Status.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ng.TraceListeners.Models
+{
+    public class Status : TableEntity
+    {
+        DateTime _recordedOn;
+
+        public Status() : this(DateTime.Now)
+        {
+        }
+
+        public Status(DateTime eventTime)
+        {
+            this.Machine = Environment.MachineName;
+            this.ThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            this.EventTime = eventTime;
+            this.Application = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            this.Activity = String.Empty;
+            this.State = String.Empty;
+            this.Result = String.Empty;
+            this.Details = String.Empty;
+            this.Level = String.Empty;
+        }
+
+        public string Machine { get; set; }
+
+        public int ThreadId { get; set; }
+        public string Activity { get; set; }
+        public DateTime EventTime
+        {
+            get
+            {
+                return this._recordedOn;
+            }
+            set
+            {
+                this._recordedOn = value;
+                this.PartitionKey = value.Date.Ticks.ToString();
+                this.RowKey = value.Ticks.ToString();
+            }
+        }
+
+        public string State { get; set; }
+        public string Result { get; set; }
+        public string Details { get; set; }
+
+        public string Application { get; set; }
+        public string Level { get; set; }
+    }
+}
---
+++ src/Ng.TraceListeners/TraceStatus.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ng.TraceListeners
+{
+    public class TraceStatus : IDisposable
+    {
+        string _activity;
+        Stopwatch _stopwatch;
+
+        public TraceStatus(string activity, string details = null)
+        {
+            if (String.IsNullOrWhiteSpace(activity))
+            {
+                throw new ArgumentNullException("activity");
+            }
+
+            this._activity = activity;
+            this._stopwatch = new Stopwatch();
+            this._stopwatch.Start();
+
+            TraceStatus.TraceInformation(activity, state: "Start", details: details);
+        }
+
+        public void Dispose()
+        {
+            this._stopwatch.Stop();
+            TraceStatus.TraceInformation(this._activity, state: "Stop", details: $"Elapsed Seconds: {this._stopwatch.Elapsed.TotalSeconds.ToString("#,###")}");
+        }
+
+        public static void TraceInformation(string activity = null, string state = null, string result = null, string details = null)
+        {
+            System.Diagnostics.Trace.TraceInformation(ComposeString(activity, state, result, details));
+        }
+
+        public static void TraceWarning(string activity = null, string state = null, string result = null, string details = null)
+        {
+            System.Diagnostics.Trace.TraceWarning(ComposeString(activity, state, result, details));
+        }
+
+        public static void TraceError(string activity = null, string state = null, string result = null, string details = null)
+        {
+            System.Diagnostics.Trace.TraceError(ComposeString(activity, state, result, details));
+        }
+
+        private static string ComposeString(string activity, string state, string result, string details)
+        {
+            StringBuilder text = new StringBuilder();
+            text.Append("#Status");
+
+            if (!String.IsNullOrWhiteSpace(activity))
+            {
+                text.Append($" #Activity={ReplaceInvalidCharacters(activity)}");
+            }
+
+            if (!String.IsNullOrWhiteSpace(state))
+            {
+                text.Append($" #State={ReplaceInvalidCharacters(state)}");
+            }
+
+            if (!String.IsNullOrWhiteSpace(result))
+            {
+                text.Append($" #Result={ReplaceInvalidCharacters(result)}");
+            }
+
+            if (!String.IsNullOrWhiteSpace(details))
+            {
+                text.Append($" #Details={ReplaceInvalidCharacters(details)}");
+            }
+
+            return text.ToString();
+        }
+
+        private static string ReplaceInvalidCharacters(string text)
+        {
+            text = text.Replace('=', '_');
+            text = text.Replace('#', '_');
+
+            return text;
+        }
+    }
+}
